e637973001d8dc0513cbfa9951d5aed6fe56103f
Ondrej Danko <xj@vaio.(none)>
2011-11-13 15:34:05 +0100

    Initialized Rails, added slovak SRS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMS::Application.config.secret_token = '7752793d801da26c1931103ab23df4b02dbf5d4a75c814158c78b846562a14d4a715be28ce9b7ce5510a6e5012494c9b73d0898715a1b6514168d6c640d755a2'
---
+++ srs/freemind.mm
@@ -0,0 +1,476 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Content management system" ID="ID_24433459" CREATED="1321002697630" MODIFIED="1321008002233" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+<hook NAME="MapStyle" max_node_width="600"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<node TEXT="System features" POSITION="right" ID="ID_1359420542" CREATED="1321005909784" MODIFIED="1321008005203" COLOR="#669900" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<node TEXT="Modules" ID="ID_149989104" CREATED="1321006255520" MODIFIED="1321040592584" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<node ID="ID_817670911" CREATED="1321005981992" MODIFIED="1321030013451" COLOR="#ff0000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Page
+    </p>
+    <p>
+      <i>basic module</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="12"/>
+<icon BUILTIN="yes"/>
+<node TEXT="create" ID="ID_1934925703" CREATED="1321006081105" MODIFIED="1321008163237" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_386448796" CREATED="1321006086320" MODIFIED="1321008164159" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_140027261" CREATED="1321006091368" MODIFIED="1321008165500" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="menu" ID="ID_746630265" CREATED="1321029811171" MODIFIED="1321029983734">
+<icon BUILTIN="attach"/>
+</node>
+<node TEXT="publish" ID="ID_702171393" CREATED="1321009717587" MODIFIED="1321029878520">
+<icon BUILTIN="calendar"/>
+</node>
+</node>
+<node ID="ID_1192055132" CREATED="1321006033088" MODIFIED="1321030045220" COLOR="#ff0000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Story
+    </p>
+    <p>
+      <i>basic module</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="12"/>
+<icon BUILTIN="yes"/>
+<node TEXT="create" ID="ID_973686651" CREATED="1321006198680" MODIFIED="1321008166523" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_1629300388" CREATED="1321006202416" MODIFIED="1321008166880" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_666357501" CREATED="1321006204512" MODIFIED="1321008167327" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="rating" ID="ID_1899116194" CREATED="1321006370632" MODIFIED="1321008099502">
+<font NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="bookmark"/>
+</node>
+<node TEXT="tags" ID="ID_1107620266" CREATED="1321009165388" MODIFIED="1321017824755">
+<icon BUILTIN="flag-yellow"/>
+</node>
+<node TEXT="commentaries" ID="ID_384825578" CREATED="1321006512744" MODIFIED="1321008124821">
+<font NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node TEXT="publish" ID="ID_205458772" CREATED="1321009717587" MODIFIED="1321029878520">
+<icon BUILTIN="calendar"/>
+</node>
+</node>
+<node ID="ID_1163864799" CREATED="1321008874028" MODIFIED="1321032392684" COLOR="#ff0000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      HomePage
+    </p>
+    <p>
+      <i><font size="3">basic module</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="12"/>
+<icon BUILTIN="yes"/>
+<node TEXT="create" ID="ID_231273306" CREATED="1321008928253" MODIFIED="1321008950114" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_910986247" CREATED="1321008942716" MODIFIED="1321008950776" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_1980397117" CREATED="1321008945012" MODIFIED="1321008951106" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node ID="ID_380068685" CREATED="1321008874028" MODIFIED="1321030030768" COLOR="#ff0000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Menu
+    </p>
+    <p>
+      <i><font size="3">basic module</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="12"/>
+<icon BUILTIN="yes"/>
+<node TEXT="create" ID="ID_1229765087" CREATED="1321008928253" MODIFIED="1321008950114" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_659619284" CREATED="1321008942716" MODIFIED="1321008950776" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_1139091686" CREATED="1321008945012" MODIFIED="1321008951106" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node ID="ID_578296015" CREATED="1321039595502" MODIFIED="1321040450314" COLOR="#ff0000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      User
+    </p>
+    <p>
+      <i><font size="3">basic module</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="12"/>
+<icon BUILTIN="yes"/>
+<node TEXT="create" ID="ID_260459452" CREATED="1321008928253" MODIFIED="1321008950114" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+<node TEXT="role" ID="ID_1411629244" CREATED="1321008567053" MODIFIED="1321039675523">
+<node TEXT="Guest" ID="ID_1533499282" CREATED="1321008578477" MODIFIED="1321008714653">
+<icon BUILTIN="group"/>
+</node>
+<node TEXT="User" ID="ID_284290207" CREATED="1321008648021" MODIFIED="1321008757660">
+<icon BUILTIN="fema"/>
+</node>
+<node TEXT="Administrator" ID="ID_1949469187" CREATED="1321008650677" MODIFIED="1321008743029">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node TEXT="profile info" ID="ID_1547870414" CREATED="1321039628846" MODIFIED="1321039633738"/>
+</node>
+<node TEXT="edit" ID="ID_997186623" CREATED="1321008942716" MODIFIED="1321008950776" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+<node TEXT="role" ID="ID_1299055843" CREATED="1321008567053" MODIFIED="1321039675523">
+<node TEXT="Guest" ID="ID_300200141" CREATED="1321008578477" MODIFIED="1321008714653">
+<icon BUILTIN="group"/>
+</node>
+<node TEXT="User" ID="ID_1529731091" CREATED="1321008648021" MODIFIED="1321008757660">
+<icon BUILTIN="fema"/>
+</node>
+<node TEXT="Administrator" ID="ID_69803158" CREATED="1321008650677" MODIFIED="1321008743029">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node TEXT="profile info" ID="ID_1254329990" CREATED="1321039628846" MODIFIED="1321039633738"/>
+</node>
+<node TEXT="delete" ID="ID_689581836" CREATED="1321008945012" MODIFIED="1321008951106" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+<node TEXT="role" ID="ID_63058034" CREATED="1321008567053" MODIFIED="1321039675523">
+<node TEXT="Guest" ID="ID_1215925192" CREATED="1321008578477" MODIFIED="1321008714653">
+<icon BUILTIN="group"/>
+</node>
+<node TEXT="User" ID="ID_365491542" CREATED="1321008648021" MODIFIED="1321008757660">
+<icon BUILTIN="fema"/>
+</node>
+<node TEXT="Administrator" ID="ID_1131076288" CREATED="1321008650677" MODIFIED="1321008743029">
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node TEXT="profile info" ID="ID_1109258267" CREATED="1321039628846" MODIFIED="1321039633738"/>
+</node>
+</node>
+<node ID="ID_653392405" CREATED="1321006320328" MODIFIED="1321040460337" COLOR="#ff0000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Authentification
+    </p>
+    <p>
+      <i><font size="3">basic module</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="12"/>
+<icon BUILTIN="yes"/>
+<node TEXT="login" ID="ID_356466876" CREATED="1321006727663" MODIFIED="1321008178488" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="logout" ID="ID_1071996215" CREATED="1321006730888" MODIFIED="1321008178894" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="registration" ID="ID_1802938229" CREATED="1321037960328" MODIFIED="1321037966691" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="forgotten password" ID="ID_1166096131" CREATED="1321037967336" MODIFIED="1321038031836" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node ID="ID_211347260" CREATED="1321006446024" MODIFIED="1321040467689" COLOR="#ff0000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Administration interface
+    </p>
+    <p>
+      <i><font size="3">basic module</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Liberation Sans" SIZE="12"/>
+<icon BUILTIN="yes"/>
+</node>
+<node ID="ID_1535782794" CREATED="1321009335004" MODIFIED="1321040539176">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Theme
+    </p>
+    <p>
+      <i>add-on module</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+<node TEXT="create" ID="ID_726170376" CREATED="1321008928253" MODIFIED="1321008950114" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_392625813" CREATED="1321008942716" MODIFIED="1321008950776" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_1545197433" CREATED="1321008945012" MODIFIED="1321008951106" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node ID="ID_750963627" CREATED="1321017401595" MODIFIED="1321040534083">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Locale
+    </p>
+    <p>
+      <i>add-on module</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+<node TEXT="create" ID="ID_1902863837" CREATED="1321008928253" MODIFIED="1321008950114" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_565065204" CREATED="1321008942716" MODIFIED="1321008950776" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_97969672" CREATED="1321008945012" MODIFIED="1321008951106" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node ID="ID_52351504" CREATED="1321006767152" MODIFIED="1321031421916">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Microsite
+    </p>
+    <p>
+      <i>add-on module </i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="idea"/>
+<node TEXT="create" ID="ID_1940404831" CREATED="1321006931919" MODIFIED="1321008172464" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_456198251" CREATED="1321006935127" MODIFIED="1321008172776" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_1074560410" CREATED="1321006936927" MODIFIED="1321008173150" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node ID="ID_491061982" CREATED="1321006557999" MODIFIED="1321008039779">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Survey
+    </p>
+    <p>
+      <i>add-on module</i>
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="idea"/>
+<node TEXT="create" ID="ID_1110901683" CREATED="1321006844623" MODIFIED="1321008169646" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="edit" ID="ID_876853770" CREATED="1321006848647" MODIFIED="1321008170074" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="delete" ID="ID_1116941675" CREATED="1321006850807" MODIFIED="1321008170504" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="tags" ID="ID_1394654871" CREATED="1321009165388" MODIFIED="1321017832644">
+<icon BUILTIN="flag-yellow"/>
+</node>
+<node TEXT="commentaries" ID="ID_681578019" CREATED="1321006854255" MODIFIED="1321008137950">
+<font NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="pencil"/>
+</node>
+<node TEXT="publish" ID="ID_1734085891" CREATED="1321009717587" MODIFIED="1321029901512">
+<icon BUILTIN="calendar"/>
+</node>
+</node>
+<node ID="ID_1175821379" CREATED="1321007749822" MODIFIED="1321040488547">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Wysiwyg editor
+    </p>
+    <p>
+      <i>add-on module</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+<node TEXT="bold" ID="ID_503888458" CREATED="1321008285981" MODIFIED="1321008369669" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="italic" ID="ID_532924725" CREATED="1321008329821" MODIFIED="1321008370100" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="underline" ID="ID_512781236" CREATED="1321008336245" MODIFIED="1321008370531" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="strike" ID="ID_1234912210" CREATED="1321045987766" MODIFIED="1321045992060" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="font-size" ID="ID_132276299" CREATED="1321008340229" MODIFIED="1321009950647" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="smiley" ID="ID_777276298" CREATED="1321045995822" MODIFIED="1321045999620" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+<node TEXT="image" ID="ID_678517925" CREATED="1321009913923" MODIFIED="1321009919370" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="12" BOLD="false"/>
+<icon BUILTIN="password"/>
+</node>
+</node>
+<node ID="ID_648087300" CREATED="1321031472393" MODIFIED="1321031522146">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Search
+    </p>
+    <p>
+      <i>add-on module</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</map>
