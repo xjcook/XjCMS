081517c0e13827a83cbfd37b5719b2c66f8f17d9
Ondrej Danko <xjcook@gmail.com>
2011-12-12 12:44:50 +0100

    Improved structure of design scss -> more dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper :application
+  helper :all
   helper_method :current_user
   before_filter :set_locale
   before_filter :authorize
@@ -16,35 +16,17 @@ class ApplicationController < ActionController::Base
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
   end
-
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
-  def authorize
-    user = User.find_by_id(session[:user_id])
-    
-    # check if user is logged
-    if user
-      # check if user has rights
+  def authorize    
+    if current_user
+      # check if user has rights      
+      hero = current_user.role.permissions.where(:hero => true).exists?
       
-      #hero = user.role.permissions.where(:hero => true).exists?
-      
-      #if current_page?(:controller => 'user')
-      #  if hero 
-      #    return true
-      #  end
-      #elsif current_page?(:controller => 'page')
-      #  if hero 
-      #    return true
-      #  end
-      #elsif current_page?(:controller => 'story')
-      #  if hero 
-      #    return true
-      #  end
-      #end
-      
-      #return false
+      # ... ?
     else
       redirect_to login_path, :notice => "Please log in!"
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,17 @@ class UsersController < ApplicationController
   skip_before_filter :authorize
 
   def new
-    @role = Role.find_by_name("user")
     @user = User.new
   end
 
   def create
-#if User.first
-    @role = Role.find_by_name("user")
+    if User.first.nil?
+      @role = Role.find_by_name("admin")
+    else
+      @role = Role.find_by_name("user")
+    end
+    
     @user = User.new(params[:user])
-#@user.role_id = 2 # normal user
-#else
-#@user = User.new(params[:user])
-#@user.role_id = 1 # administrator
-#end
     
     if @user.save
       redirect_to root_url, :notice => "Signed up!"
