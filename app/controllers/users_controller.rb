453ee62034096051d64c53a71650d6a254503c58
Ondrej Danko <xjcook@gmail.com>
2011-12-14 01:13:24 +0100

    Fixed a few bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,24 @@ class UsersController < ApplicationController
   # TODO respond_to
 
   def new
-    if User.first.nil?
-      @role = Role.find_by_name("admin")
-    else
-      @role = Role.find_by_name("user")
+    if current_user.nil?
+      if User.first.nil?
+        @role = Role.find_by_name("admin")
+      else
+        @role = Role.find_by_name("user")
+      end
+      @user = User.new
+    elsif
+      redirect_to root_path, :notice => "You are already signed!"
     end
-    
-    @user = User.new
   end
 
   def create   
-    @user = User.new(params[:user])
     @role = Role.find(params[:user][:role_id])
+    @user = User.new(params[:user])
     
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      redirect_to root_path, :notice => "Signed up!"
     else
       render "new"
     end
