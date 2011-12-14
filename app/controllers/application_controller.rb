ac20aeeffdc66b2d2a5f3aaa1ee3fb37cfca630f
Ondrej Danko <xjcook@gmail.com>
2011-12-14 08:20:34 +0100

    Improved usability: redirect back after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,50 +22,51 @@ class ApplicationController < ActionController::Base
   
   def authorize(options={})
     if current_user.nil?
+      session[:redirect_back] = url_for(:only_path => false)
       redirect_to login_path, :notice => "Please log in!"
       return false
+    else
+      hero_right     = current_user.role.permissions.where(:hero => true).exists?
+      pages_right    = current_user.role.permissions.where(:pages => true).exists?
+      stories_right  = current_user.role.permissions.where(:stories => true).exists?
+      comments_right = current_user.role.permissions.where(:comments => true).exists?
+  
+      # Check rights
+      # 1. check section where we are
+      # 2. check if we have right to this section
+      # 3. check if user is hero -> can edit/destroy all posts
+      #                  or he is creating new post (doesn't have id)
+      #          else if user is author -> can edit/destroy own post
+      # if we have right then return true, else redirect to root_path and return false
+      case options[:section]
+        when :pages then
+          if pages_right
+            if hero_right || params[:id].nil? 
+              return true
+            elsif Page.find(params[:id]).user.id == current_user.id
+              return true
+            end
+          end
+        when :stories then
+          if stories_right
+            if hero_right || params[:id].nil? 
+              return true
+            elsif Story.find(params[:id]).user.id == current_user.id
+              return true
+            end
+          end
+        when :comments then
+          if comments_right
+            if hero_right || params[:id].nil? 
+              return true
+            elsif Comment.find(params[:id]).user.id == current_user.id
+              return true
+            end
+          end
+      end    
+       
+      redirect_to root_path, :notice => "You don't have rights!"
+      return false
     end
-
-    hero_right     = current_user.role.permissions.where(:hero => true).exists?
-    pages_right    = current_user.role.permissions.where(:pages => true).exists?
-    stories_right  = current_user.role.permissions.where(:stories => true).exists?
-    comments_right = current_user.role.permissions.where(:comments => true).exists?
-
-    # Check rights
-    # 1. check section where we are
-    # 2. check if we have right to this section
-    # 3. check if user is hero -> can edit/destroy all posts
-    #                  or he is creating new post (doesn't have id)
-    #          else if user is author -> can edit/destroy own post
-    # if we have right then return true, else redirect to root_path and return false
-    case options[:section]
-      when :pages then
-        if pages_right
-          if hero_right || params[:id].nil? 
-            return true
-          elsif Page.find(params[:id]).user.id == current_user.id
-            return true
-          end
-        end
-      when :stories then
-        if stories_right
-          if hero_right || params[:id].nil? 
-            return true
-          elsif Story.find(params[:id]).user.id == current_user.id
-            return true
-          end
-        end
-      when :comments then
-        if comments_right
-          if hero_right || params[:id].nil? 
-            return true
-          elsif Comment.find(params[:id]).user.id == current_user.id
-            return true
-          end
-        end
-    end    
-     
-    redirect_to root_path, :notice => "You don't have rights!"
-    return false
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,12 @@ class SessionsController < ApplicationController
     user = User.find_by_login(params[:login])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
-      #redirect_to :back
+      
+      if session[:redirect_back].nil?
+        redirect_to root_url, :notice => "Logged in!"
+      else
+        redirect_to session[:redirect_back], :notice => "Logged in!"
+      end
     else
       flash.now.alert = "Invalid email or password"
       render "new"
@@ -19,6 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    session[:redirect_back] = nil
     redirect_to root_url, :notice => "Logged out!"
   end
 end
